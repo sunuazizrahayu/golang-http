a0142f8403f516ea78328c7d4a3089c57d294063
Sunu Aziz Rahayu <me@kangsunu.com>
2023-11-19 08:51:59 +0700

    add load .env

--- main.go
+++ main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"golang-http/routes"
 	"log"
+	"os"
 )
 
 func main() {
+	//load config
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(err)
+	}
+
+	//gofiber app
 	app := fiber.New()
 
 	//init route
 	routes.RouteApp(app)
 
-	log.Fatal(app.Listen(":3000"))
+	appPort := os.Getenv("APP_PORT")
+	if appPort == "" {
+		appPort = "3000"
+	}
+	log.Fatal(app.Listen(":" + appPort))
 }
